70d8ad2e4794638313a145529524ed35d129e25d
Andrei Popa <andrei.popa@eu.equinix.com>
2024-09-18 12:30:19 +0200

    change the fcr module

--- variables.tf
+++ variables.tf
@@ -1,24 +1,10 @@
 variable "name" {
-  type    = string
   default = ""
 }
-variable "email" {
-  type    = string
-  default = ""
-}
-variable "purchase_order_number" {
-  type    = string
-  default = ""
-}
-variable "project_id" {
-  type    = string
-  default = ""
-}
-variable "account_number" {
-  type    = string
-  default = ""
-}
-variable "location" {
-  type    = string
-  default = ""
-}
+variable "type" {}
+variable "metro_code" {}
+variable "emails" {}
+variable "purchase_order_number" {}
+variable "code" {}
+variable "project_id" {}
+variable "account_number" {}
