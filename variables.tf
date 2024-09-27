81dcaaccb7fc1b32ecf53aeb862cc310af6f329a
vipin2060 <vipinvikraman86@gmail.com>
2024-09-27 18:14:19 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,3 +8,7 @@ variable "purchase_order_number" {}
 variable "code" {}
 variable "project_id" {}
 variable "account_number" {}
+
+#27
+variable "vipin_client_id" {}
+variable "vipin_client_secret" {}
